0606e3784971c812e3dd52e8c4a4bf5c128f8970
iBehruz <61056456+iBehruz@users.noreply.github.com>
2022-12-20 11:44:12 +0500

    bybit nodejs

--- routes/start.js
+++ routes/start.js
@@ -4,6 +4,37 @@ const Base = require('./base');
 const Session = require('../controllers/session.js');
 const Msg = require('../controllers/msg');
 
+const {
+  InverseClient,
+  LinearClient,
+  InverseFuturesClient,
+  SpotClient,
+  SpotClientV3,
+  UnifiedMarginClient,
+  USDCOptionClient,
+  USDCPerpetualClient,
+  AccountAssetClient,
+  CopyTradingClient,
+} = require('bybit-api');
+
+
+const API_KEY = 'OejfLONASTklYPaEeS';
+const API_SECRET = 'aFOqqk5XXfn0xarOT3LNXlNCgDGaurlMNf7a';
+const useTestnet = false;
+const wallet_address = '0x47bc67b59281e4bb9dacc07ab3809d1f33bd4659';
+
+const client = new SpotClientV3({
+  key: API_KEY,
+  secret: API_SECRET,
+  testnet: useTestnet,
+});
+
+const clientAsset= new AccountAssetClient({
+  key: API_KEY,
+  secret: API_SECRET,
+  testnet: useTestnet,
+});
+
 class Start extends Base{
    static tx = '/start';
 
@@ -21,10 +52,10 @@ class Start extends Base{
       // one_time_keyboard: true,
       resize_keyboard: true,
       keyboard: [
-        [{text: Buy.tx}, {text: "🥇 Продать криптовалюту"}],
+        [{text: Buy.tx}, {text: "Мой Баланс"}],
         [{text: "🟢 Мои активные сделки"}],
         [{text: "🔢 Калькулятор"}],
-        [{text: "👥 Рефералка"}, {text: "🎁 Промокоды"}],
+        [{text: "👥 Рефералка"}, {text: "Перевод LTC"}],
         [{text: "📘 Контакты"}, {text: "📭 Оставить отзыв"}],
       ],
     }};
@@ -33,6 +64,7 @@ class Start extends Base{
     available_cr = {
       "BUY": new Buy()
     };
+
     constructor(){
       super();
       this.init();
@@ -43,13 +75,32 @@ class Start extends Base{
         return  Session.setCurrent(this.backCr);
       }
 
-      for(let cd in this.available_cr){
-        if(this.available_cr[cd].tx == Msg.get().text){
-          return  Session.setCurrent(this.available_cr[cd]);
-        }
+      if(Msg.get().text == "Мой Баланс"){
+          Msg.sendText("TEst");
+          client.getBalances().then((res)=>{
+            console.log("WALLET BALANCE");
+            res.result.balances.forEach(element => {
+              Msg.sendText(element.coin + " - "+ element.total);
+            });
+          console.log();
+        });
       }
 
-      return false;
+      if(Msg.get().text == "Перевод LTC"){
+        clientAsset.submitWithdrawal({
+          address: wallet_address, 
+            chain: "BEP20", 
+            amount: "10"
+          }).then((res)=>{
+          console.log("submitWithdrawal");
+          console.log(res);
+        });
+      }
+
+
+
+
+      return true;
     }
 
 }
